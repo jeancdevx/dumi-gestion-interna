78cd943dfb9e46527bfc3453440935582103e088
jeancdev <jcode2006@gmail.com>
2025-10-08 17:16:46 -0500

    feat(components): enhance responsive dialog with scroll

--- components/responsive-dialog.tsx
+++ components/responsive-dialog.tsx
@@ -23,6 +23,7 @@ interface ResponsiveDialogProps {
   children: React.ReactNode
   open: boolean
   onOpenChange: (open: boolean) => void
+  maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl'
 }
 
 const ResponsiveDialog = ({
@@ -30,22 +31,35 @@ const ResponsiveDialog = ({
   description,
   children,
   open,
-  onOpenChange
+  onOpenChange,
+  maxWidth = 'md'
 }: ResponsiveDialogProps) => {
   const isMobile = useIsMobile()
+
+  const maxWidthClasses = {
+    sm: 'max-w-sm',
+    md: 'max-w-md',
+    lg: 'max-w-lg',
+    xl: 'max-w-xl',
+    '2xl': 'max-w-2xl',
+    '3xl': 'max-w-3xl',
+    '4xl': 'max-w-4xl'
+  }
 
   if (isMobile) {
     return (
       <Drawer open={open} onOpenChange={onOpenChange}>
-        <DrawerContent>
-          <DrawerHeader>
+        <DrawerContent className='max-h-[90vh]'>
+          <DrawerHeader className='shrink-0'>
             <DrawerTitle className='text-center text-2xl'>{title}</DrawerTitle>
             <DrawerDescription className='text-center text-xs'>
               {description}
             </DrawerDescription>
           </DrawerHeader>
 
-          <div className='p-4'>{children}</div>
+          <div className='min-h-0 flex-1 space-y-6 overflow-y-auto p-4'>
+            {children}
+          </div>
         </DrawerContent>
       </Drawer>
     )
@@ -53,8 +67,10 @@ const ResponsiveDialog = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className='space-y-4'>
-        <DialogHeader>
+      <DialogContent
+        className={`flex max-h-[90vh] flex-col overflow-hidden ${maxWidthClasses[maxWidth]}`}
+      >
+        <DialogHeader className='shrink-0 pb-4'>
           <DialogTitle className='text-center text-xl md:text-3xl'>
             {title}
           </DialogTitle>
@@ -63,7 +79,9 @@ const ResponsiveDialog = ({
           </DialogDescription>
         </DialogHeader>
 
-        {children}
+        <div className='min-h-0 flex-1 space-y-6 overflow-y-auto px-1 pr-2'>
+          {children}
+        </div>
       </DialogContent>
     </Dialog>
   )
