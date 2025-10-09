324a30eace0e779b7d1927853248652655f47570
jeancdev <jcode2006@gmail.com>
2025-10-09 03:11:15 -0500

    feat(admin): add customer schema and update customer schema

--- modules/admin/schemas.ts
+++ modules/admin/schemas.ts
@@ -16,5 +16,24 @@ export const updateEmployeeSchema = employeeSchema.extend({
   id: z.string()
 })
 
+const regexPhone = /^(?:\+51)?[ -]?9\d{8}$/
+
+export const customerSchema = z.object({
+  firstName: z.string().min(1, 'El nombre es obligatorio').trim(),
+  lastName: z.string().min(1, 'El apellido es obligatorio').trim(),
+  phone: z
+    .string()
+    .min(1, 'El teléfono es obligatorio')
+    .regex(regexPhone, 'El teléfono debe ser un número válido de Perú')
+    .trim(),
+  reference: z.string().optional()
+})
+
+export const updateCustomerSchema = customerSchema.extend({
+  id: z.string()
+})
+
 export type EmployeeFormData = z.infer<typeof employeeSchema>
 export type UpdateEmployeeFormData = z.infer<typeof updateEmployeeSchema>
+export type CustomerFormData = z.infer<typeof customerSchema>
+export type UpdateCustomerFormData = z.infer<typeof updateCustomerSchema>
