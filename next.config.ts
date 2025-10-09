ff6b9c2150afd27efb0466385eef785b58d5e32f
jeancdev <jcode2006@gmail.com>
2025-10-09 12:41:48 -0500

    feat(config): add image configuration for remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets-dumi-dev.edducode.me',
+        pathname: '/**'
+      }
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '30mb' // 5 images * 5MB each + form data
