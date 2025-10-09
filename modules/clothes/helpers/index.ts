14238aa506f36c05b5ff75fae407e8b686f3e0ba
jeancdev <jcode2006@gmail.com>
2025-10-09 12:40:44 -0500

    feat(clothes): add helper functions

---
+++ modules/clothes/helpers/index.ts
@@ -0,0 +1,20 @@
+import { CLOTHES_GENDER } from '../types'
+
+export const getSizeApiValue = (sizeName: string): string => sizeName
+
+export const getGenderApiValue = (genderName: string): string => {
+  const genderMap: Record<string, string> = {
+    hombre: CLOTHES_GENDER.MALE,
+    mujer: CLOTHES_GENDER.FEMALE,
+    unisex: CLOTHES_GENDER.UNISEX
+  }
+
+  return genderMap[genderName] || CLOTHES_GENDER.UNISEX
+}
+
+export const formatPrice = (price: number): string => {
+  return new Intl.NumberFormat('es-PE', {
+    style: 'currency',
+    currency: 'PEN'
+  }).format(price)
+}
